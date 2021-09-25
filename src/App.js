4bf60337224e32386e4c6921f325849295820c3a
iazadur <iamazadur@gmail.com>
2021-09-26 01:21:07 +0600

    update all

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import Header from './components/Header/Header';
+import Specialist from './components/Specialist/Specialist';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      
+      <Specialist></Specialist>
     </div>
   );
 }
---
+++ src/components/Specialist/Specialist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Appointment from '../Appointment/Appointment';
+import Doctors from '../Doctors/Doctors';
+
+const Specialist = () => {
+    return (
+        <div>
+            <Doctors></Doctors>
+            <Appointment></Appointment>
+        </div>
+    );
+};
+
+export default Specialist;
